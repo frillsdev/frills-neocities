599914d6dd5d57526165f57dbb3bd6b3089be0bd
Tegan <tegan.mm@googlemail.com>
2023-06-04 23:48:25 +0100

    🎉 Init commit

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+module.exports = {
+	title: "frills' internet garden",
+	url: "https://example.com/",
+	language: "en-GB",
+	description: "full of weeds and encroaching moss",
+	author: {
+		name: "frills",
+		email: "",
+		url: ""
+	}
+}
