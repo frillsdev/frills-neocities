d55a58d69df2412f5b0651c8aedc1554e6fa8bb3
Tegan <tegan.mm@googlemail.com>
2023-08-13 16:18:38 +0100

    🐛  Fix webrings missing from Home

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,9 @@
 // Add notification bunotife to nav items
 function notification(navItem, hiddenText) {
-    const notifNavItem = navItem
-    const notifSRText = hiddenText
-    const notifTarget = document.getElementsByClassName('page__nav')[0].getElementsByTagName('UL')[0].children.item(notifNavItem)
-
+    const notifTarget = document.getElementsByClassName('page__nav')[0].getElementsByTagName('UL')[0].children.item(navItem)
     const notif = document.createElement('span')
     notif.classList.add('notification')
-    notif.innerHTML = notifSRText
+    notif.innerHTML = hiddenText
     notifTarget.append(notif)
 }
 
