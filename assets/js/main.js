f2b3bdbcc752050ba10ae60b02b1dde73785c020
Tegan <tegan.mm@googlemail.com>
2023-09-30 12:33:55 +0100

    🎃 Add spookify

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,6 +35,7 @@ function colorScheme() {
             colorScheme = 'dark';
         }
     }
+
     function setColorScheme(colorScheme) {
         document.documentElement.dataset.scheme = colorScheme;
         if (colorScheme == 'light') {
@@ -55,8 +56,62 @@ function colorScheme() {
     });
 }
 
+// Spookify
+function storeEmoji() {
+    const emojis = Array.from(document.getElementsByClassName('emoji'))
+    emojis.map((emoji) => {
+        emoji.dataset.emoji = emoji.innerText;
+    })
+}
+
+function restoreEmoji() {
+    const emojis = Array.from(document.getElementsByClassName('emoji'))
+    emojis.map((emoji) => {
+        emoji.innerText = emoji.dataset.emoji
+    })
+}
+
+function changeEmoji() {
+    const emojis = Array.from(document.getElementsByClassName('emoji'))
+    const newEmojiSet = ['🎃', '👻', '💀', '😱', '🧟', '🧙', '🐺', '🧛', '🕷️', '🕸️', '🦉', '🦇', '🌕', '🍫', '🍬', '🍭', '⚰️', '🥀', '🩸']
+    emojis.map((emoji) => {
+        const randomIndex = Math.floor(Math.random() * newEmojiSet.length);
+        emoji.innerText = newEmojiSet[randomIndex]
+        emoji.setAttribute('aria-hidden', true)
+    })
+}
+
+function spookyScheme() {
+    const spookyButton = document.getElementById('spookyButton');
+    let spookify = localStorage.getItem('spookify');
+
+    function setSpookyScheme() {
+        document.documentElement.dataset.spooky = spookify;
+        if (spookify === 'true') {
+            spookyButton.setAttribute('aria-pressed', true);
+            changeEmoji();
+        } else {
+            spookyButton.setAttribute('aria-pressed', false);
+            restoreEmoji();
+        }
+    }
+    setSpookyScheme();
+
+    spookyButton.addEventListener('click', () => {
+        if (spookify === 'false') {
+            spookify = 'true';
+        } else {
+            spookify = 'false';
+        }
+        localStorage.setItem('spookify', spookify);
+        setSpookyScheme();
+    });
+}
+
 window.addEventListener('load', function () {
     // notification(3, 'New!');
     consoleClub();
     colorScheme();
+    storeEmoji();
+    spookyScheme();
 }, true);
