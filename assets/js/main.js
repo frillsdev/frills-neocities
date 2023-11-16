a2ae782a2ec6a699e4fa703e21fa339deb3677a4
Tegan Male <tmale@spindogs.com>
2023-11-16 19:18:12 +0000

    ✨ Add random avatar

--- assets/js/main.js
+++ assets/js/main.js
@@ -70,9 +70,83 @@ function sparkles() {
     });
 }
 
+
+const randomiser = (array, currentNumber) => {
+    let newNumber = Math.floor(Math.random() * array.length);
+    if (newNumber === currentNumber) {
+        newNumber = Math.floor(Math.random() * array.length)
+    }
+    return newNumber
+}
+
+function avatarSwapper() {
+    const avatars = [
+        {   src: "/images/picrew/PotatoLord’s_Persona_Creator.png",
+            alt: "Cute af pic",
+            credit: "PotatoLord’s_Persona_Creator",
+            crediturl: "https://picrew.me/en/image_maker/1473879"
+        },
+        {   src: "/images/picrew/makowka_character_maker_II.png",
+            alt: "Cute af pic",
+            credit: "makowka",
+            crediturl: "https://picrew.me/en/image_maker/644129"
+        },
+        {   src: "/images/picrew/crows_character_creator_.png",
+            alt: "Cute af pic",
+            credit: "crowwithapen",
+            crediturl: "https://picrew.me/en/image_maker/1243146"
+        },
+        {   src: "/images/picrew/Ultimate_friends_face_maker.png",
+            alt: "Cute af pic",
+            credit: "makowka_character_maker_II",
+            crediturl: "https://picrew.me/en/image_maker/1342558"
+        },
+        {   src: "/images/picrew/OC_Creator-transparent.png",
+            alt: "Cute af pic",
+            credit: "MaeEllen",
+            crediturl: "https://picrew.me/en/image_maker/2141620"
+        },
+        {   src: "/images/picrew/flowerveil.png",
+            alt: "Cute af pic",
+            credit: "flowerveil",
+            crediturl: "http://asdad.com"
+        },
+        {   src: "/images/picrew/miaw-sq.jpg",
+            alt: "Cute af pic",
+            credit: "Mia Wilkes",
+            crediturl: "http://mia-wilkes.squarespace.com/"
+        },
+        {   src: "/images/picrew/stardew.png",
+            alt: "Cute af pic",
+            credit: "Jazzybee",
+            crediturl: "https://jazzybee.itch.io/sdvcharactercreator"
+        },
+        // {   src: "/images/picrew/TOON_ME_⟪_B_⟫.png",
+        //     alt: "Cute af pic",
+        //     credit: "TOON_ME_⟪_B_⟫",
+        //     crediturl: "http://asdad.com"
+        // },
+    ]
+    let diceRoll = randomiser(avatars)
+    const avatarContainer = document.getElementsByClassName('swapAvatar')[0]
+    const avatar = avatarContainer.appendChild(document.createElement('img'));
+    avatar.id='swappedAvatar';
+    avatar.src=avatars[diceRoll].src;
+    avatar.alt=avatars[diceRoll].alt;
+    avatarContainer.classList.add('swapped');
+    document.getElementById('refreshAvatar').addEventListener("click", (event) => {
+        let reRoll = randomiser(avatars, diceRoll)
+        let avatar = document.getElementById('swappedAvatar')
+        avatar.src=avatars[reRoll].src;
+        avatar.alt=avatars[reRoll].alt;
+    });
+}
+
 window.addEventListener('load', function () {
     // notification(4, 'New!');
     consoleClub();
     colorScheme();
     sparkles();
 }, true);
+
+avatarSwapper()
