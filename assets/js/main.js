47b431bf5d088f0fab033c4f0d7c08500fb5327d
Tegan <tegan.mm@googlemail.com>
2023-11-17 10:06:07 +0000

    🧩 Add Collections (stickers and avatars)
    * ✨ Add avatar randomiser

--- assets/js/main.js
+++ assets/js/main.js
@@ -70,9 +70,63 @@ function sparkles() {
     });
 }
 
+
+const randomiser = (array, currentNumber) => {
+    let randomNumber = Math.floor(Math.random() * array.length);
+    if (randomNumber === currentNumber) { randomNumber = Math.floor(Math.random()* array.length) }
+    return randomNumber
+}
+
+function avatarSwapper() {
+    const avatars = [
+        {   src: "/images/collections/avatars/PotatoLords_Persona_Creator.png",
+            alt: "Avatar of a white woman with brown hair, eyes closed, tongue sticking out. Wearing an orange jumper with a sun on it.",
+        },
+        {   src: "/images/collections/avatars/makowka_character_maker_II.png",
+            alt: "Avatar or a woman with brown hair, styled in spacebuns and blonde streaks where men would have sideburns. Wearing blue dungarees with a red jumper with strawberries on it. ",
+        },
+        {   src: "/images/collections/avatars/crows_character_creator_.png",
+            alt: "Avatar of a white woman with messy brown hair, and blonde streaks where men would have sideburns. Surprised expression with eyes wide and mouth open. Wearing an orange jumper with a rainbow LGBTQIA+ badge.",
+        },
+        {   src: "/images/collections/avatars/Ultimate_friends_face_maker.png",
+            alt: "Avatar of a white woman with brown hair and blonde streaks where men would have sideburns. Surly expression with slighly drooped eyes and straight lips. Wearing blue dungarees with a green jumper.",
+        },
+        {   src: "/images/collections/avatars/nosk.png",
+            alt: "Avatar of a white woman with brown hair, happy expression and chunky eyebrows. Muted blueish colour palette, wearing blue dungarees and a grey top. The background is blue spotted with white polka dots",
+        },
+        {   src: "/images/collections/avatars/OC_Creator-transparent.png",
+            alt: "Avatar or a woman with brown hair, styled in spacebuns and blonde streaks where men would have sideburns. Wearing metal armour.",
+        },
+        {   src: "/images/collections/avatars/flowerveil.png",
+            alt: "Avatar or a woman with brown ombre hair, excessively big eyelashes and sparkly eyes. Sparkles surround her.",
+        },
+        {   src: "/images/collections/avatars/miaw.jpg",
+            alt: "Avatar of a white woman with brown hair and pink cheeks. Wearing yellow dungarees and a grey stripey top. She is framed by a branch of leaves starting from the bottom branching up to the left and right.",
+        },
+        {   src: "/images/collections/avatars/stardew.png",
+            alt: "Avatar of a white woman with brown hair tucked behind her left ear. Wearing pink dungarees and a pink top. In the style of Stardew Valley.",
+        },
+    ]
+    let diceRoll = randomiser(avatars)
+    const avatarContainer = document.getElementsByClassName('swapAvatar')[0]
+    const avatar = avatarContainer.appendChild(document.createElement('img'));
+    avatar.id='swappedAvatar';
+    avatar.src=avatars[diceRoll].src;
+    avatar.alt=avatars[diceRoll].alt;
+    avatarContainer.classList.add('swapped');
+    document.getElementById('refreshAvatar').addEventListener("click", (event) => {
+        let reRoll = randomiser(avatars, diceRoll)
+        let avatar = document.getElementById('swappedAvatar')
+        avatar.src=avatars[reRoll].src;
+        avatar.alt=avatars[reRoll].alt;
+    });
+}
+
 window.addEventListener('load', function () {
-    // notification(4, 'New!');
+    notification(3, 'New!');
     consoleClub();
     colorScheme();
     sparkles();
 }, true);
+
+avatarSwapper()
