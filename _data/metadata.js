c37d11889a7e5495e328c6ab015e26116abbe9cf
Tegan Male <tmale@spindogs.com>
2024-02-19 14:28:05 +0000

    🔍 Add metadata and OG image

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,10 +1,11 @@
 module.exports = {
 	title: "Frills",
 	url: "https://frills.dev",
+	avatar: "https://frills.dev/images/collections/avatars/andycarolan-pink.png",
 	language: "en-GB",
 	description: "",
 	author: {
-		name: "frills",
+		name: "Frills",
 		email: "fri11s@proton.me",
 		url: "https://frills.dev"
 	}
