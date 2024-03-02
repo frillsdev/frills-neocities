13065fadd6b7ca23d792100ffd927c37df3b4822
Tegan <tegan.mm@googlemail.com>
2024-03-02 02:07:55 +0000

    🔍️ Update meta data

--- _data/metadata.js
+++ _data/metadata.js
@@ -3,7 +3,7 @@ module.exports = {
 	url: "https://frills.dev",
 	avatar: "https://frills.dev/images/collections/avatars/andycarolan-pink.png",
 	language: "en-GB",
-	description: "",
+	description: "Personal website of Frills",
 	author: {
 		name: "Frills",
 		email: "fri11s@proton.me",
