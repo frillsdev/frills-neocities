1ebabea241023c46a695e56101791d4367a595ec
Tegan Male <tmale@spindogs.com>
2024-04-11 19:36:19 +0100

    💬 Add webmentions

--- _data/webmentions.js
+++ _data/webmentions.js
@@ -1,5 +1,5 @@
-console.log('TESTING')
 const fs = require("fs");
+const fetch = require("node-fetch");
 
 fetch('https://webmention.io/api/mentions.jf2?token=jnh-mOU3cWyJYca2T4x3vg')
   .then(response => response.json())
