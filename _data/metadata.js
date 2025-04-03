fcf4a5fdd878aa216587952ca9e1a0719846199f
Frills <tegan.mm@googlemail.com>
2025-04-03 01:11:39 +0100

    📄 Update nocache

--- _data/metadata.js
+++ _data/metadata.js
@@ -2,7 +2,7 @@ module.exports = {
 	title: "Frills",
 	url: "https://frills.dev",
 	avatar: "https://frills.dev/images/avatars/avatar.png",
-	nocache: "curios",
+	nocache: "🧼",
 	language: "en-GB",
 	description: "Personal website of Frills",
 	author: {
